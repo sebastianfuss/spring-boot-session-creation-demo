4febb076c4ff84fe89032d06e0eaccba256ffe74
Sebastian Fuß <sebastian.fuss@dwins.de>
2022-08-22 21:41:12 +0200

    deprecated spring security configuration with extending WebSecurityConfigurerAdapter added

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -1,12 +1,18 @@
 package com.example.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.security.oauth2.resource.servlet.OAuth2ResourceServerAutoConfiguration
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration
 import org.springframework.boot.autoconfigure.websocket.servlet.WebSocketServletAutoConfiguration
 import org.springframework.boot.runApplication
 
 @SpringBootApplication(
     exclude = [
         WebSocketServletAutoConfiguration::class,
+        OAuth2ResourceServerAutoConfiguration::class,
+        SecurityAutoConfiguration::class,
+        UserDetailsServiceAutoConfiguration::class,
     ],
 )
 class DemoApplication
--- src/main/kotlin/com/example/demo/DemoSecurityConfiguration.kt
+++ src/main/kotlin/com/example/demo/DemoSecurityConfiguration.kt
@@ -1,15 +1,32 @@
 package com.example.demo
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication
 import org.springframework.boot.autoconfigure.security.oauth2.resource.servlet.OAuth2ResourceServerAutoConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Import
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.config.web.servlet.invoke
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter
 import org.springframework.security.web.SecurityFilterChain
 
+
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(
+    prePostEnabled = true,
+    securedEnabled = true,
+    jsr250Enabled = true,
+)
+@ConditionalOnWebApplication
+@ConditionalOnProperty(
+    prefix = "web.security",
+    name = ["enabled"],
+    havingValue = "true",
+    matchIfMissing = true,
+)
 @Import(OAuth2ResourceServerAutoConfiguration::class)
 class DemoSecurityConfiguration {
 
@@ -27,8 +44,51 @@ class DemoSecurityConfiguration {
                 authorize(anyRequest, denyAll)
             }
 
+            oauth2ResourceServer {
+                jwt {
+                    jwtAuthenticationConverter = JwtAuthenticationConverter()
+                }
+            }
+
         }
         return http.build()
     }
 }
+
+//@EnableWebSecurity
+//@EnableGlobalMethodSecurity(
+//    prePostEnabled = true,
+//    securedEnabled = true,
+//    jsr250Enabled = true,
+//)
+//@ConditionalOnWebApplication
+//@ConditionalOnProperty(
+//    prefix = "web.security",
+//    name = ["enabled"],
+//    havingValue = "true",
+//    matchIfMissing = true,
+//)
+//@Import(OAuth2ResourceServerAutoConfiguration::class)
+//class DemoSecurityConfiguration : WebSecurityConfigurerAdapter() {
+//
+//    override fun configure(http: HttpSecurity) {
+//        http
+//            .csrf().disable()
+//            .formLogin().disable()
+//            .logout().disable()
+//            .jee().disable()
+//            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//
+//        http {
+//            authorizeRequests {
+//                authorize(anyRequest, denyAll)
+//            }
+//            oauth2ResourceServer {
+//                jwt {
+//                    jwtAuthenticationConverter = JwtAuthenticationConverter()
+//                }
+//            }
+//        }
+//    }
+//}
 
